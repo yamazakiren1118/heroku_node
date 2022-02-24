e8856df57280d9fdc41f4f02b64de97ab738941d
yamazakiren1118 <yamazakiren1118@yahoo.co.jp>
2022-02-24 19:06:29 +0900

    socket

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var http = require('http').createServer();
+var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 http.listen(port,function(){
-  console.log('listen the node js');
+  console.log('listen the node js listen the node js listen the node js listen the node js');
 });

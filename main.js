2e716a61f847877d421f507153c2b4bf7997c66d
yamazakiren1118 <yamazakiren1118@yahoo.co.jp>
2022-02-24 19:17:14 +0900

    cors

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var http = require('http').createServer();
-var io = require('socket.io')(http);
+var io = require('socket.io')(http,{
+  cors:{
+    origin: ["http://localhost:8000", "http://127.0.0.1:8000"],
+    methods: ["GET", "POST", "DELETE", "OPTIONS"]
+  }
+});
 var port = process.env.PORT || 3000;
 http.listen(port,function(){
   console.log('listen the node js listen the node js listen the node js listen the node js');
